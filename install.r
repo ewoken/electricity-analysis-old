418111ad3d1bf6481e851dab1a0bf1767aa9a519
ewoken <ewokenfr@gmail.com>
2020-04-05 19:07:48 +0200

    test2

--- install.r
+++
@@ -1,4 +0,0 @@
-print("Check dependencies...")
-list.of.packages <- c("ggplot2", "tidyverse", "lubridate", "rgl","latex2exp")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
